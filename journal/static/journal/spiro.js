402703279dbae599c0410bcf1ae410d216f70f5f
Ritchieluk <33823229+Ritchieluk@users.noreply.github.com>
2019-04-24 17:34:18 -0400

    Finished Drawer
    - color changes
    - json reading
    - test files

--- journal/static/journal/spiro.js
+++ journal/static/journal/spiro.js
@@ -11,7 +11,7 @@ var spiro = (function (input) {
 	}
 	function load(dict, canvas){
 
-		var emotionValues = JSON.parse(decodeHtml(dict));
+		var emotionValues = JSON.parse(dict);
 		input.emotions = emotionValues["Overall"]["emotion"];
 		input.sentiment = emotionValues["Overall"]["sentiment"];
 		for (i= 0; i < emotionValues["Sentences"]["emotion"].length; i++){
