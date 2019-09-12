2c2e2cfb937090d83569ee0f03d27885520a882a
Luke D Ritchie <Ritchieluk@gmail.com>
2019-09-12 11:31:04 -0400

    User Page work

---
+++ ui/src/components/user/JournalOverview.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-expansion-panels>
+    <v-expansion-panel
+      v-for="(item,i) in 5"
+      :key="i"
+    >
+    <v-card><v-img max-height="100" src="https://via.placeholder.com/300"></v-img></v-card>
+    
+      <v-expansion-panel-header class="headline">Journal Entry {{i}}<v-spacer></v-spacer>Date <v-spacer></v-spacer></v-expansion-panel-header>
+      <v-expansion-panel-content>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- ui/src/pages/User.vue
+++ ui/src/pages/User.vue
@@ -8,11 +8,20 @@
         @toggle="drawer=!drawer"/>
         <v-content>
              <v-container>
-                <v-layout>
-                    <v-flex>
+                <v-layout wrap>
+                    <v-flex md12>
                         <v-card>
                             <v-card-title class="headline">Profile Page</v-card-title>
                         </v-card>
+                    </v-flex>
+                    <v-flex md6 class="pa-2">
+                        <v-card>
+                            <v-card-title>Profile Information</v-card-title>
+                            <v-img max-width="300" aspect-ratio="1" src="https://via.placeholder.com/300"></v-img>
+                        </v-card>
+                    </v-flex>
+                    <v-flex md6>
+                        <Overview/>
                     </v-flex>
                 </v-layout>
             </v-container>
@@ -25,6 +34,7 @@
 import Menu from "../components/common/Menu";
 import Toolbar from "../components/common/Toolbar";
 import Footer from "../components/common/Footer";
+import JournalOverview from "@/components/user/JournalOverview";
 
 export default {
     data: ()=>({
@@ -33,7 +43,8 @@ export default {
     components: {
         Menu: Menu,
         Toolbar: Toolbar,
-        Footer: Footer
+        Footer: Footer,
+        Overview: JournalOverview
     },
     methods: {
         checkDrawer: function(value){
