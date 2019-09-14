687862f14dcf25fbfbbfa01b9376804e148c93d9
Luke D Ritchie <Ritchieluk@gmail.com>
2019-09-14 14:34:00 -0400

    Sign Up Form Created

--- ui/src/pages/SignUp.vue
+++ ui/src/pages/SignUp.vue
@@ -1,19 +1,24 @@
 <template>
     <v-container>
-        <v-layout>
-            <v-flex>
+        <v-layout align-center justify-center wrap>
+            <v-flex md8>
                 <v-card>
                     <v-card-title class="headline">Sign Up Page</v-card-title>
                 </v-card>
+            </v-flex>
+            <v-flex md6>
+                <SignUpForm/>
             </v-flex>
         </v-layout>
     </v-container>
 </template>
 
 <script>
-
+import SignUpForm from "@/components/SignUpForm";
 export default {
-    
+    components: {
+        SignUpForm
+    }
 }
 </script>
 
